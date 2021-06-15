298087eccb4952dd22f4ec880d2ebe5f285f2ebf
Pedro Ricardo <ordepricardo13@gmail.com>
2021-06-15 19:44:44 -0300

    first commit

---
+++ aluraframe/client/js/lib/index.js
@@ -0,0 +1,32 @@
+const campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor'),
+]
+
+console.log(campos)
+
+const tbody = querySelector('table tbody')
+
+const form = document.querySelector('.form')
+form.addEventListener('submit', function() {
+    preventDefault()
+    var tr = document.createElement('tr')
+    
+    campos.forEach(function(campo) {
+        const td = document.createElement('td')
+        td.textContent = campo.nodeValue
+        tr.appendChild(td)
+    })
+    const tdVolume = document.createElement('td')
+    tdVolume.textContent = campos[1].value * campos[2].value
+
+    tr.appendChild(tdVolume)
+
+    tbody.appendChild(tr)
+
+    campos[0].value = ''
+    campos[1].value = 1
+    campos[2].value = 0
+    campos[0].focus()
+})
